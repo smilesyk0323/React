231b792bd61fb28deef92a862f3049b8c9218bed
smilesyk0323 <smilesyk0323@naver.com>
2023-11-02 21:43:59 +0900

    코드 정리

--- demo03/src/components/Exam10.js
+++ demo03/src/components/Exam10.js
@@ -24,6 +24,14 @@ const Exam10 = () => {
         itemType: ""
     });
 
+    //(중요) "시작하자마자" items의 내용을 backup으로 복제(1회) 
+    useEffect(() => {
+        setBackup(items.map(item => {
+            const newItem = { ...item };
+            return newItem;
+        }));
+    }, []);
+
     //useRef : 특정 대상을 참조할 수 있는 훅
     // - const 이름 = useRef(초기값);
     // - 태그에 ref라는 속성으로 이름을 지정해두면 언제든지 불러서 사용할 수 있다
@@ -36,111 +44,6 @@ const Exam10 = () => {
             [e.target.name]: e.target.value
         };
         setData(newData);
-    };
-
-    //(중요) "시작하자마자" items의 내용을 backup으로 복제(1회) 
-    useEffect(() => {
-        setBackup(items.map(item => {
-            const newItem = { ...item };
-            return newItem;
-        }));
-    }, []);
-
-    //줄을 수정상태로 변경하는 함수
-    //- 이 함수를 실행하려면 최소한 itemNo는 알아야 한다
-    //- 함수를 호출할 때 이벤트 정보(e) 대신 아이템 정보(item)을 전달하여 처리하도록 처리
-    const changeToEdit = (target) => {//넘겨받은 item으로 수정
-        // console.log(item);    
-
-
-    //아이템 변경
-    const newItems = items.map(item => {
-        if (item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
-                return {
-                    ...item,//다른건 그대로 둬라 (item을 펼쳐라)
-                    edit: true
-                };
-            }
-            return item;//나머진 현상유지
-        });
-
-        setItems(newItems);//items를 변경해서 새로 만든 배열
-    };
-
-    //줄의 데이터를 변경하는 함수
-    //- 어떤 아이템인지(target)와 뭐라고 했는지(e)를 알아야 한다
-    const changeItem = (target, e) => {
-        const newItems = items.map(item => {
-            if (item.itemNo === target.itemNo) {//같은 번호를 발견한다면
-                return {
-                    ...item,//나머지 정보는 그대로 두고
-                    [e.target.name]: e.target.value//입력창의 이름에 해당하는 필드값을 입력값으로 바꿔라
-                }
-            }
-            return item;
-        });
-        setItems(newItems);
-    };
-
-    //취소버튼을 누른 경우 실행할 함수
-    //- backup에 들어있는 target과 번호가 같은 데이터를 items의 target과 같은 번호에 덮어쓰기
-    const cancelItem = (target) => {
-
-        //backup에서 target의 번호에 해당하는 객체를 찾는다(filter)
-        const findResult = backup.filter(item => item.itemNo === target.itemNo);
-        // console.log(findResult);
-
-        //아이템 변경
-        const newItems = items.map(item => {
-            if (item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
-                return {
-                    ...findResult[0],//다른건 백업데이터로 두고
-                    edit: false//edit를 false로바꿔라
-                };
-            }
-            return item;
-        });
-        setItems(newItems);//items를 변경해서 새로 만든 배열
-
-    };
-
-    const saveItem = (target) => {
-
-        //백업 데이터 중 target과 번호가 같은 데이터를 갱신
-        const newBackup = backup.map(item => {
-            if (item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
-                return {
-                    ...target,//변경된 데이터로 저장하고 
-                    edit: false//edit을 false로 바꿔라
-                };
-            }
-            return item;
-        });
-        setBackup(newBackup);
-
-        const newItems = items.map(item => {
-            if (item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
-                return {
-                    ...item,//다른건 그대로 둬라 (item을 펼쳐라)
-                    edit: false
-                };
-            }
-            return item;
-        });
-        setItems(newItems);//items를 변경해서 새로 만든 배열
-
-    };
-
-    //아이템 삭제 
-    //- 배열에서 항목을 삭제할 때도 filter를 사용한다
-    const deleteItem = (target) => {
-        //아이템 삭제
-        const newItems = items.filter(item => item.itemNo !== target.itemNo);
-        setItems(newItems);
-
-        //백업 삭제 
-        const newBackup = backup.filter(item => item.itemNo !== target.itemNo);
-        setBackup(newBackup);
     };
 
     //아이템 추가
@@ -185,6 +88,109 @@ const Exam10 = () => {
         closeModal();
     };
 
+
+
+    //수정 버튼 (줄을 수정상태로 변경하는 함수)
+    //- 이 함수를 실행하려면 최소한 itemNo는 알아야 한다
+    //- 함수를 호출할 때 이벤트 정보(e) 대신 아이템 정보(item)을 전달하여 처리하도록 처리
+    const changeToEdit = (target) => {//넘겨받은 item으로 수정
+        // console.log(item);  화면에서 console에서 해당 정보가 들어오는지 확인  
+
+        //아이템 변경
+        // const newItems = items.map(item => item);
+        const newItems = items.map(item => {
+            if (item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
+                    return {
+                        ...item,//다른건 그대로 둬도 (item을 펼쳐라)
+                        edit: true
+                    };
+                }
+                return item;//나머진 현상유지
+            });
+
+        setItems(newItems);//items를 변경해서 새로 만든 배열
+    };
+
+    //수정버튼을 누르고 입력값을 변경하는 함수! 
+    //줄의 데이터를 변경하는 함수
+    //- 어떤 아이템인지(target)와 뭐라고 했는지(e)를 알아야 한다
+    const changeItem = (target, e) => {
+        const newItems = items.map(item => {
+            if (item.itemNo === target.itemNo) {//같은 번호를 발견한다면
+                return {
+                    ...item,//나머지 정보는 그대로 두고
+                    [e.target.name]: e.target.value//입력창의 이름에 해당하는 필드값을 입력값으로 바꿔라
+                }
+            }
+            return item;
+        });
+        setItems(newItems);
+    };
+
+    //취소버튼을 누른 경우 실행할 함수
+    //- backup에 들어있는 target과 번호가 같은 데이터를 items의 target과 같은 번호에 덮어쓰기
+    const cancelItem = (target) => {
+
+        //backup에서 target의 번호에 해당하는 객체를 찾는다(filter)
+        const findResult = backup.filter(item => item.itemNo === target.itemNo);
+        // console.log(findResult);
+
+        //아이템 변경
+        const newItems = items.map(item => {
+            if (item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
+                return {
+                    ...findResult[0],// 다른건 백업데이터로 두고
+                    //배열에 데이터가 1개만 있다면 0번 위치에 있음!
+                    edit: false//edit를 false로바꿔라
+                };
+            }
+            return item;
+        });
+        setItems(newItems);//items를 변경해서 새로 만든 배열
+
+    };
+
+    //완료버튼
+    const saveItem = (target) => {
+
+        //백업 데이터 중 target과 번호가 같은 데이터를 갱신
+        const newBackup = backup.map(item => {
+            if (item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
+                return {
+                    ...target,//변경된 데이터로 저장하고 
+                    edit: false//edit을 false로 바꿔라
+                };
+            }
+            return item;
+        });
+        setBackup(newBackup);
+
+        const newItems = items.map(item => {
+            if (item.itemNo === target.itemNo) {//target과 같은 번호의 상품만큼은
+                return {
+                    ...item,//다른건 그대로 둬라 (item을 펼쳐라)
+                    edit: false
+                };
+            }
+            return item;
+        });
+        setItems(newItems);//items를 변경해서 새로 만든 배열
+
+    };
+
+    //아이템 삭제 
+    //- 배열에서 항목을 삭제할 때도 filter를 사용한다
+    const deleteItem = (target) => {
+        //아이템 삭제
+        const newItems = items.filter(item => item.itemNo !== target.itemNo);
+        setItems(newItems);
+
+        //백업 삭제 
+        const newBackup = backup.filter(item => item.itemNo !== target.itemNo);
+        setBackup(newBackup);
+    };
+
+    
     //모달창 취소 버튼
     const cancelAddItem = ()=>{
                 //입력창 초기화
