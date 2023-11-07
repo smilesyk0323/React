8fce946b991b1dc99f4d0be7ffa97f23b14ea3e7
smilesyk0323 <smilesyk0323@naver.com>
2023-11-07 19:08:09 +0900

    저장

--- demo05/src/App.js
+++ demo05/src/App.js
@@ -5,26 +5,24 @@ import Home from './components/Home';
 import Menu from './components/Menu';
 import BookInfinite from './components/BookInfinite';
 
-
 function App() {
   return (
     <div className="container-fluid my-5 py-5">
-        {/* 상단 메뉴 영역 */}
-        <Menu/>
-    
-        {/* 본문 영역 */}
-        <div className='row'>
-            {/* <div className=' col-sm-10 offset-sm-1'> */}
-            <div className='col'>
-            <Routes>
-              <Route exact path="/" element={<Home/>}></Route>
-              <Route path="/pocketmon" element={<Pocketmon/>}></Route>
-              <Route path="/book" element={<Book/>}></Route>
-              <Route path="/book2" element={<BookInfinite/>}></Route>
-            </Routes>
-            </div>
+      {/* 상단 메뉴 영역 */}
+      <Menu />
+      
+      {/* 본문 영역 */}
+      <div className='row'>
+        {/* <div className=' col-sm-10 offset-sm-1'> */}
+        <div className='col'>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/pocketmon" element={<Pocketmon />} />
+            <Route path="/book" element={<Book />} />
+            <Route path="/book2" element={<BookInfinite />} />
+          </Routes>
         </div>
-    
+      </div>
     </div>
   );
 }
